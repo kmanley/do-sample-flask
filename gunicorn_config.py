ac3fa22a25685763bbfe90b11e51d86b1aa933c9
Kevin Manley <kevin.manley@gmail.com>
2025-09-17 11:47:01 -0400

    more files

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,2 +1,7 @@
 bind = "0.0.0.0:8080"
 workers = 2
+loglevel = "info"
+#accesslog = "/var/log/gunicorn/access.log"
+#errorlog = "/var/log/gunicorn/error.log"
+accesslogformat = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+capture_output = True
